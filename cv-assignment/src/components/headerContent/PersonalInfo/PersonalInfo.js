26dc3e48776463afd953b07a14024472e1010df0
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 19:15:12 +0200

    delete unsed imports

--- cv-assignment/src/components/headerContent/PersonalInfo/PersonalInfo.js
+++ cv-assignment/src/components/headerContent/PersonalInfo/PersonalInfo.js
@@ -1,30 +1,9 @@
 import React from "react";
-import {
-  CalendarOutlined,
-  EnvironmentOutlined,
-  LinkedinOutlined,
-  GithubOutlined,
-  MailOutlined,
-  PhoneOutlined
-} from "@ant-design/icons";
-import { List, Row, Col } from "antd";
+import { Row, Col } from "antd";
 import PersonalData from "./PersonalData";
 import ContactIcons from "./ContactIcons";
 
 const PersonalInfo = () => {
-  const getPersonalData = () => {
-    return [
-      { label: "20/05/1993", icon: CalendarOutlined },
-      { label: "Limbiate (MB)", icon: EnvironmentOutlined }
-    ];
-  };
-
-  const getContactIcons = () => [
-    {icon: LinkedinOutlined},
-    {icon:GithubOutlined},
-    {icon:MailOutlined},
-    {icon:PhoneOutlined},
-  ];
   return (
     <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
       <Col className="gutter-row" span={8}>
