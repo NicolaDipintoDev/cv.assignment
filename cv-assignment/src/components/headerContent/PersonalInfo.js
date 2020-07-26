19f8ecec8ddbf5da813a079e7505984ad53e73a9
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 18:00:00 +0200

    import List

--- cv-assignment/src/components/headerContent/PersonalInfo.js
+++ cv-assignment/src/components/headerContent/PersonalInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { CalendarOutlined, EnvironmentOutlined } from "@ant-design/icons";
+import { List } from  'antd';
 
 const PersonalInfo = () => <div>personal info</div>;
 
