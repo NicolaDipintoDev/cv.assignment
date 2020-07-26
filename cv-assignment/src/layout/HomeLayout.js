769723d5dbddd81dada8373dcd0f7b6bc16e293d
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 17:26:45 +0200

    create HomeLayout

---
+++ cv-assignment/src/layout/HomeLayout.js
@@ -0,0 +1,12 @@
+import { Layout } from "antd";
+
+const { Header, Content } = Layout;
+
+const HomeLayout = () => (
+  <Layout>
+    <Header>Header</Header>
+    <Content>Content</Content>
+  </Layout>
+);
+
+export default HomeLayout;
