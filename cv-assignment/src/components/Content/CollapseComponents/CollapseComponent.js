acfe42dbe55c2f5c062ce69b34c37452dec89e6b
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 20:00:47 +0200

    add propTypes

--- cv-assignment/src/components/Content/CollapseComponents/CollapseComponent.js
+++ cv-assignment/src/components/Content/CollapseComponents/CollapseComponent.js
@@ -1,14 +1,20 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import { Collapse } from "antd";
 
 const { Panel } = Collapse;
 
-const CollapseComponent = ({title}) => (
+const CollapseComponent = ({title, contentPanel}) => (
     <Collapse defaultActiveKey={["1"]} ghost>
       <Panel header={<h1>{title}</h1>} key="1">
-        panel 1
+        {contentPanel}
       </Panel>
     </Collapse>
 );
 
 export default CollapseComponent;
+
+CollapseComponent.propTypes = {
+    title: PropTypes.string.isRequired,
+    contentPanel: PropTypes.element.isRequired
+  };
