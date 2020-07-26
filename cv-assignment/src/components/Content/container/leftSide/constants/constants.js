5e1469b28a69e6c82ffdb7a91610aa01b56f6671
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 23:04:43 +0200

    add swEngineering list

--- cv-assignment/src/components/Content/container/leftSide/constants/constants.js
+++ cv-assignment/src/components/Content/container/leftSide/constants/constants.js
@@ -1,11 +1,26 @@
 export const languages = {
-    bg: '#FAD563',
-    color: '#132B45',
-    data: ['JavaScript', 'PHP', 'Java', 'TypeScript', 'MySql', 'Html', 'Css', 'Less']
-}
+  bg: "#FAD563",
+  color: "#132B45",
+  data: [
+    "JavaScript",
+    "PHP",
+    "Java",
+    "TypeScript",
+    "MySql",
+    "Html",
+    "Css",
+    "Less"
+  ]
+};
 
 export const frameworks = {
-    bg: '#132B45',
-    color: '#FAD563',
-    data: ['React', 'Laravel', 'Angular', 'Bootstrap']
-}
+  bg: "#132B45",
+  color: "#FAD563",
+  data: ["React", "Laravel", "Angular", "Bootstrap"]
+};
+
+export const swEngineering = {
+  bg: "#80929E",
+  color: "#132B45",
+  data: ["Clean Code", "Refactoring", "UX", "Design Patterns", "MVC", "UML"]
+};
