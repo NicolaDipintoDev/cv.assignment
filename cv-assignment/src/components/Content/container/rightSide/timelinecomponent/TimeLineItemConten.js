197e82f6b47be1fc0da59385951ffc7d31c15868
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 21:16:33 +0200

    add TimeLineItemContent propTypes

--- cv-assignment/src/components/Content/container/rightSide/timelinecomponent/TimeLineItemConten.js
+++ cv-assignment/src/components/Content/container/rightSide/timelinecomponent/TimeLineItemConten.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const TimeLineItemContent = ({ contents }) => (
   <div>
@@ -9,3 +10,7 @@ const TimeLineItemContent = ({ contents }) => (
 );
 
 export default TimeLineItemContent;
+
+TimeLineItemContent.propTypes = {
+  contents: PropTypes.array.isRequired,
+};
