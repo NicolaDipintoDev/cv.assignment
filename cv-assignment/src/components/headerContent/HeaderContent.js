a2bdba6fdee1fafb57f1f6bb382cc61a21d63ba7
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 17:56:21 +0200

    render PersonalInfo

--- cv-assignment/src/components/headerContent/HeaderContent.js
+++ cv-assignment/src/components/headerContent/HeaderContent.js
@@ -1,8 +1,13 @@
 import React from "react";
-import PersonalInfo from './PersonalInfo';
+import PersonalInfo from "./PersonalInfo";
 
-const HeaderContent = () => <div className="headerContent">
-     <h1 className="HeaderName">Nicola Dipinto</h1>
-     </div>;
+const HeaderContent = () => (
+  <div className="headerContent">
+    <h1 className="HeaderName">Nicola Dipinto</h1>
+    <div>
+      <PersonalInfo />
+    </div>
+  </div>
+);
 
 export default HeaderContent;
