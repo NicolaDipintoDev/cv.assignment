3d4599c9427819920eec2282faba6a95cc74446f
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 22:40:01 +0200

    add skillsgrid

--- cv-assignment/src/components/Content/container/leftSide/skill/Skills.js
+++ cv-assignment/src/components/Content/container/leftSide/skill/Skills.js
@@ -3,9 +3,23 @@ import Skill from "./Skill";
 import { List } from "antd";
 
 const Skills = () => {
-  const skills = [{ name: "react", bg: "#FAD563", color: "#132B45" }];
+  const skills = [
+    { name: "react", bg: "#FAD563", color: "#132B45" },
+    { name: "react", bg: "#FAD563", color: "#132B45" },
+    { name: "react", bg: "#FAD563", color: "#132B45" },
+    { name: "react", bg: "#FAD563", color: "#132B45" },
+    { name: "react", bg: "#FAD563", color: "#132B45" },
+    { name: "react", bg: "#FAD563", color: "#132B45" }
+  ];
   return (
     <List
+      grid={{
+        gutter: 16,
+        xs: 5,
+        sm:6,
+        md: 8, 
+        lg: 10
+      }}
       size="small"
       dataSource={skills}
       split={false}
