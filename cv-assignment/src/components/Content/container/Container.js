8742c8c601217ea5eac01c81f5b5ea0c8e797464
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 22:09:00 +0200

    render LeftSide

--- cv-assignment/src/components/Content/container/Container.js
+++ cv-assignment/src/components/Content/container/Container.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Row, Col } from "antd";
 import RightSide from "./rightSide/RightSide";
+import LeftSide from "./leftSide/LeftSide";
 
 
 const Container = () => (
   <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
     <Col className="gutter-row" xs={24} sm={12}>
-      left side
+      <LeftSide />
     </Col>
     <Col className="gutter-row" xs={24} sm={12}>
       <RightSide />
