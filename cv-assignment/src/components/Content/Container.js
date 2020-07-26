a33d256a0a7007d2d30d6f00419fe4a552b24e2e
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 19:43:37 +0200

    add Container colums

--- cv-assignment/src/components/Content/Container.js
+++ cv-assignment/src/components/Content/Container.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Row, Col } from "antd";
 
-const Container = () => <div>Content container</div>
+const Container = () => (
+  <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+    <Col className="gutter-row" xs={24} sm={12}>
+      left side
+    </Col>
+    <Col className="gutter-row" xs={24} sm={12}>
+      right side
+    </Col>
+  </Row>
+);
 
 export default Container;
