8faa9a7d8d1beb438c78a85063a13d25bc09ca33
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 22:00:25 +0200

    experiences consntants real implementation

--- cv-assignment/src/components/Content/container/rightSide/constants/constants.js
+++ cv-assignment/src/components/Content/container/rightSide/constants/constants.js
@@ -2,14 +2,63 @@ export const education = {
   title: "Education",
   experiences: [
     {
-      date: "01/2020 - now",
-      title: "Company name",
-      description: "skdsjkdjskdjskjdksjdskjdskjdskjdskjdks"
+      date: "09/2019 - 10/2019",
+      title: "OverNet Education",
+      description: "Java Academy"
     },
     {
-      date: "03/2020 - now",
-      title: "company name",
-      description: "askdsjkdjskdjskjdksjdskjdskjdskjdskjdks"
+      date: "03/2019",
+      title: "Università degli Studi di Milano-Bicocca",
+      description: "Bachelor’s degree in Computer Science"
     }
+  ]
+};
+
+export const workExperiences = {
+  title: "Work Experiences",
+  experiences: [
+    {
+      date: "01/2020 - now",
+      title: "FME Education",
+      description: `Full stack engineer
+        React, Laravel, MySql, MongoDB.
+        Front-end: components development in TTD with react.
+        Back-end: DPI development in TTD with Laravel.`
+    },
+    {
+      date: "10/2019 - 11/2019",
+      title: "FME Education",
+      description: `Full stack engineer
+          React, Laravel, MySql, MongoDB.
+          Front-end: components development in TTD with react.
+          Back-end: DPI development in TTD with Laravel.`
+    },
+    {
+      date: "01/2020 - now",
+      title: "Accenture",
+      description: "Java Developer"
+    },
+    {
+      date: "04/2016 - 09/2019",
+      title: "Formazione Amica ADV",
+      description: `Private courses of programming: c/c++, Java, html, CSS, javascript;
+        Private lessons of math and computer science
+        to the artistic gymnastic national team, from
+        the first to the last year of highschool;
+        Private lessons to middle school, highschool and university students.
+        `
+    },
+    {
+      date: "01/2014 - 02/2018",
+      title: "Pizzeria Graziella",
+      description: `Client and orders management; 
+        Pizza baking management.
+          `
+    },
+    {
+        date: "07/2013 - 12/2013",
+        title: "Fastweb",
+        description: 'Network monitoring;'
+      }
   ]
 };
