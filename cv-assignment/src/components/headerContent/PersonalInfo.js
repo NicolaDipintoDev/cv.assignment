17629afc3e265f9f50d2db3b87e3e94e31dfe351
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 18:03:48 +0200

    add GetPersonalData

--- cv-assignment/src/components/headerContent/PersonalInfo.js
+++ cv-assignment/src/components/headerContent/PersonalInfo.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { CalendarOutlined, EnvironmentOutlined } from "@ant-design/icons";
-import { List } from  'antd';
+import { List } from "antd";
 
-const PersonalInfo = () => <div>personal info</div>;
+const PersonalInfo = () => {
+  const getPersonalData = () => {
+    return [
+      { label: "20/05/1993", icon: CalendarOutlined },
+      { label: "Limbiate (MB)", icon: EnvironmentOutlined }
+    ];
+  };
+  return <div>personal info</div>;
+};
 
 export default PersonalInfo;
