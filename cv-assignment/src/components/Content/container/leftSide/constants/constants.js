4d4387e2ccae8f7bfa663fa5b58dfe840677cd99
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 23:02:14 +0200

    add frameworks

--- cv-assignment/src/components/Content/container/leftSide/constants/constants.js
+++ cv-assignment/src/components/Content/container/leftSide/constants/constants.js
@@ -3,3 +3,9 @@ export const languages = {
     color: '#132B45',
     data: ['JavaScript', 'PHP', 'Java', 'TypeScript', 'MySql', 'Html', 'Css', 'Less']
 }
+
+export const frameworks = {
+    bg: '#132B45',
+    color: '#FAD563',
+    data: ['React', 'Laravel', 'Angular', 'Bootstrap']
+}
