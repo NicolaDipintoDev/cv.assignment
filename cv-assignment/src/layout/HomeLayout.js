655fb4eea67b180e9704400cc1f7cfbe47bd85e3
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 18:23:52 +0200

    add layout.css

--- cv-assignment/src/layout/HomeLayout.js
+++ cv-assignment/src/layout/HomeLayout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Layout } from "antd";
 import HeaderContent from "../components/headerContent/HeaderContent";
+import './layout.css';
 
 const { Header, Content } = Layout;
 
 const HomeLayout = () => (
-  <Layout>
+  <Layout className='layout'>
     <Header>
       <HeaderContent />
     </Header>
