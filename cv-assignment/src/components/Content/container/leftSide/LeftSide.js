114b3970aaee5dc77affbd88dfd24ed0e0589d08
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 22:34:41 +0200

    partial implementation Skills

--- cv-assignment/src/components/Content/container/leftSide/LeftSide.js
+++ cv-assignment/src/components/Content/container/leftSide/LeftSide.js
@@ -1,12 +1,8 @@
 import React from "react";
-import Skill from "./skill/Skill";
 import Skills from "./skill/Skills";
 
 const LeftSide = () => (
   <div>
-    LeftSide
-    <Skill name='react' bg='#FAD563' color='#132B45'/>
-    <Skill name='react' bg='#FAD563' color='#132B45'/>
     <Skills />
   </div>
 );
--- cv-assignment/src/components/Content/container/leftSide/skill/Skills.js
+++ cv-assignment/src/components/Content/container/leftSide/skill/Skills.js
@@ -1,6 +1,20 @@
 import React from "react";
+import Skill from "./Skill";
+import { List } from "antd";
 
-const Skills = () => (
-  <div>Skilld</div>
-);
+const Skills = () => {
+  const skills = [{ name: "react", bg: "#FAD563", color: "#132B45" }];
+  return (
+    <List
+      size="small"
+      dataSource={skills}
+      split={false}
+      renderItem={item => (
+        <List.Item>
+          <Skill name={item.name} bg={item.bg} color={item.color} />
+        </List.Item>
+      )}
+    />
+  );
+};
 export default Skills;
