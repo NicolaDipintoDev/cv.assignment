2616e21446a70c406fdbf5644b6f8705feb75751
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 22:30:08 +0200

    render Skills

--- cv-assignment/src/components/Content/container/leftSide/LeftSide.js
+++ cv-assignment/src/components/Content/container/leftSide/LeftSide.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Skill from "./skill/Skill";
+import Skills from "./skill/Skills";
 
 const LeftSide = () => (
   <div>
     LeftSide
     <Skill name='react' bg='#FAD563' color='#132B45'/>
     <Skill name='react' bg='#FAD563' color='#132B45'/>
+    <Skills />
   </div>
 );
 export default LeftSide;
