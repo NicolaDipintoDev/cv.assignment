0719ecdbcc1eb193c85ed7811077cb6609359f9b
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 22:17:32 +0200

    badge color

--- cv-assignment/src/components/Content/container/leftSide/skill/Skills.js
+++ cv-assignment/src/components/Content/container/leftSide/skill/Skills.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { Badge } from "antd";
 
-const Skill = () => <Badge count={"React"} style={{ backgroundColor: '#FAD563' }} />;
+const Skill = () => <Badge count={"React"} style={{ backgroundColor: '#FAD563', color:'#132B45' }} />;
 export default Skill;
