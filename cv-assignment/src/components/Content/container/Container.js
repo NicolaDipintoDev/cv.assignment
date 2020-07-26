5f3922d00178fceb8de4c3958dbe96edee9883de
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 23:51:19 +0200

    fix responsivity

--- cv-assignment/src/components/Content/container/Container.js
+++ cv-assignment/src/components/Content/container/Container.js
@@ -6,11 +6,15 @@ import LeftSide from "./leftSide/LeftSide";
 
 const Container = () => (
   <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-    <Col className="gutter-row" xs={24} sm={12}>
+    <Col className="gutter-row" xs={0} sm={0} md={2} lg={4}>
+    </Col>
+    <Col className="gutter-row" xs={24} sm={12} md={10} lg={8}>
       <LeftSide />
     </Col>
-    <Col className="gutter-row" xs={24} sm={12}>
+    <Col className="gutter-row" xs={24} sm={12} md={10} lg={8}>
       <RightSide />
+    </Col>
+    <Col className="gutter-row" xs={0} sm={0} md={2} lg={4}>
     </Col>
   </Row>
 );
