31b6e1eaa301c6443922f552be69c63ae28604cb
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 20:24:42 +0200

    add temp TimeLineItems

--- cv-assignment/src/components/Content/container/rightSide/TimeLineComponent.js
+++ cv-assignment/src/components/Content/container/rightSide/TimeLineComponent.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Timeline } from "antd";
 
 const TimeLineComponent = () => (
-  <div>TimeLineComponent</div>
+  <Timeline>
+    <Timeline.Item color="yellow">
+      Create a services site 2015-09-01
+    </Timeline.Item>
+    <Timeline.Item color="yellow">
+      Create a services site 2015-09-01
+    </Timeline.Item>
+  </Timeline>
 );
 
 export default TimeLineComponent;
-
