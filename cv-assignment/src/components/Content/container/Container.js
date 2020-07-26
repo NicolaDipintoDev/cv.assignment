98747c0f0f81bba665005836dd9e929179165305
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 21:14:05 +0200

    move Timeline components

--- cv-assignment/src/components/Content/container/Container.js
+++ cv-assignment/src/components/Content/container/Container.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Row, Col } from "antd";
-import TimeLineComponent from "./rightSide/TimeLineComponent";
+import TimeLineComponent from "./rightSide/timelinecomponent/TimeLineComponent";
 
 const Container = () => (
   <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
