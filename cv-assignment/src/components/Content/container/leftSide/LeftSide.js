43d491ce111e8f46f649e4e0a29e6b929fcd1823
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-26 23:31:34 +0200

    render Message

--- cv-assignment/src/components/Content/container/leftSide/LeftSide.js
+++ cv-assignment/src/components/Content/container/leftSide/LeftSide.js
@@ -2,22 +2,26 @@ import React from "react";
 import SkillsBlock from "./skill/SkillsBlock";
 import { languages, frameworks, swEngineering } from "./constants/constants";
 import { List } from "antd";
+import Message from "./message/Message";
 
 const LeftSide = () => (
-  <List
-    size="small"
-    dataSource={[languages, frameworks, swEngineering]}
-    split={false}
-    renderItem={item => (
-      <List.Item>
-        <SkillsBlock
-          title={item.name}
-          skills={item.data}
-          bg={item.bg}
-          color={item.color}
-        />
-      </List.Item>
-    )}
-  />
+  <>
+    <Message />
+    <List
+      size="small"
+      dataSource={[languages, frameworks, swEngineering]}
+      split={false}
+      renderItem={item => (
+        <List.Item>
+          <SkillsBlock
+            title={item.name}
+            skills={item.data}
+            bg={item.bg}
+            color={item.color}
+          />
+        </List.Item>
+      )}
+    />
+  </>
 );
 export default LeftSide;
