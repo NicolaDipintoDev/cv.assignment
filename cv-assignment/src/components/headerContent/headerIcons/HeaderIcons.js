ca2b223fd5e7905059437cd8e61295fabca10364
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-27 20:47:52 +0200

    create HeaderIcons

---
+++ cv-assignment/src/components/headerContent/headerIcons/HeaderIcons.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { CalendarOutlined, EnvironmentOutlined } from "@ant-design/icons";
+import { List } from "antd";
+import HeaderIcon from "../headerIcons/headerIcon/HeaderIcon";
+import LinkedIcon from "../headerIcons/headerIcon/LinkedIcon";
+
+const HeaderIcons = () => {
+  const icons = [
+    { message: "20/05/1993", icon: CalendarOutlined },
+    { message: "Limbiate (MB)", icon: EnvironmentOutlined },
+    {
+      icon: LinkedinOutlined,
+      message: "LinkedIn",
+      url: "https://www.linkedin.com/in/nicola-dipinto-893b18195/"
+    },
+    {
+      icon: GithubOutlined,
+      message: "GitHub",
+      url: "https://github.com/NicolaDipintoDev"
+    },
+    {
+      icon: MailOutlined,
+      message: "dipinto.nicola.dev@gmail.com",
+      url: "mailto:dipinto.nicola.dev@gmail.com"
+    },
+    { icon: PhoneOutlined, message: "Call me", url: "tel:+393914618137" }
+  ];
+
+  const renderIcon = (index, item) =>
+    index < 2 ? (
+      <HeaderIcon Icon={item.icon} message={item.message} />
+    ) : (
+      <LinkedIcon Icon={item.icon} message={item.message} url={item.url} />
+    );
+
+  return (
+    <List
+      size="small"
+      dataSource={icons}
+      grid={{
+        gutter: 16,
+        xs: 2
+      }}
+      split={false}
+      renderItem={(item, i) => (
+        <List.Item>
+          {renderIcon(i, item)}
+        </List.Item>
+      )}
+    />
+  );
+};
+
+export default HeaderIcons;
