68489f46f810a661a419062c4ef25018163db409
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-27 21:13:40 +0200

    add logged state

--- cv-assignment/src/layout/HomeLayout.js
+++ cv-assignment/src/layout/HomeLayout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "antd";
 import HeaderContent from "../components/headerContent/HeaderContent";
 import "./layout.css";
@@ -6,15 +6,18 @@ import Container from "../components/Content/container/Container";
 
 const { Header, Content } = Layout;
 
-const HomeLayout = () => (
-  <Layout className="layout">
-    <Header style={{ height: "auto" }}>
-      <HeaderContent />
-    </Header>
-    <Content style={{ backgroundColor: "white" }}>
-      <Container />
-    </Content>
-  </Layout>
-);
+const HomeLayout = () => {
+  const [logged, setLogged] = useState(false);
+  return (
+    <Layout className="layout">
+      <Header style={{ height: "auto" }}>
+        <HeaderContent />
+      </Header>
+      <Content style={{ backgroundColor: "white" }}>
+        <Container />
+      </Content>
+    </Layout>
+  );
+};
 
 export default HomeLayout;
