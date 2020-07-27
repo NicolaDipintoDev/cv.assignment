d907c73b9181333390b94658ea8ab846fe8d981f
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-27 21:41:29 +0200

    pass logged as props

--- cv-assignment/src/components/Content/Block/Block.js
+++ cv-assignment/src/components/Content/Block/Block.js
@@ -2,11 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import CollapseComponent from "../CollapseComponents/CollapseComponent";
 
-const Block = ({ Component, title, items, bg = null, color = null }) => (
+const Block = ({
+  Component,
+  title,
+  items,
+  bg = null,
+  color = null,
+  logged
+}) => (
   <div>
     <CollapseComponent
       title={title}
-      contentPanel={<Component items={items} bg={bg} color={color} />}
+      contentPanel={
+        <Component items={items} bg={bg} color={color} logged={logged} />
+      }
     />
   </div>
 );
@@ -18,7 +27,8 @@ Block.propTypes = {
   items: PropTypes.array.isRequired,
   bg: PropTypes.string,
   color: PropTypes.string,
-  Component: PropTypes.func.isRequired
+  Component: PropTypes.func.isRequired,
+  logged: PropTypes.bool.isRequired
 };
 
 Block.defaultProps = {
