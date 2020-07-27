03cc42c4d10f95a66c3d1af17ab84ade512ea6d5
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-27 20:55:44 +0200

    export icons as constant

--- cv-assignment/src/components/headerContent/headerIcons/HeaderIcons.js
+++ cv-assignment/src/components/headerContent/headerIcons/HeaderIcons.js
@@ -1,38 +1,10 @@
 import React from "react";
-import {
-  CalendarOutlined,
-  EnvironmentOutlined,
-  LinkedinOutlined,
-  GithubOutlined,
-  MailOutlined,
-  PhoneOutlined
-} from "@ant-design/icons";
 import { List } from "antd";
 import HeaderIcon from "../headerIcons/headerIcon/HeaderIcon";
 import LinkedIcon from "../headerIcons/headerIcon/LinkedIcon";
+import { icons } from "./constants/constants";
 
 const HeaderIcons = () => {
-  const icons = [
-    { message: "20/05/1993", icon: CalendarOutlined },
-    { message: "Limbiate (MB)", icon: EnvironmentOutlined },
-    {
-      icon: LinkedinOutlined,
-      message: "LinkedIn",
-      url: "https://www.linkedin.com/in/nicola-dipinto-893b18195/"
-    },
-    {
-      icon: GithubOutlined,
-      message: "GitHub",
-      url: "https://github.com/NicolaDipintoDev"
-    },
-    { icon: PhoneOutlined, message: "Call me", url: "tel:+393914618137" },
-    {
-      icon: MailOutlined,
-      message: "dipinto.nicola.dev@gmail.com",
-      url: "mailto:dipinto.nicola.dev@gmail.com"
-    }
-  ];
-
   const renderIcon = (index, item) =>
     index < 2 ? (
       <HeaderIcon Icon={item.icon} message={item.message} />
---
+++ cv-assignment/src/components/headerContent/headerIcons/constants/constants.js
@@ -0,0 +1,29 @@
+import {
+    CalendarOutlined,
+    EnvironmentOutlined,
+    LinkedinOutlined,
+    GithubOutlined,
+    MailOutlined,
+    PhoneOutlined
+  } from "@ant-design/icons";
+  
+export const icons = [
+    { message: "20/05/1993", icon: CalendarOutlined },
+    { message: "Limbiate (MB)", icon: EnvironmentOutlined },
+    {
+      icon: LinkedinOutlined,
+      message: "LinkedIn",
+      url: "https://www.linkedin.com/in/nicola-dipinto-893b18195/"
+    },
+    {
+      icon: GithubOutlined,
+      message: "GitHub",
+      url: "https://github.com/NicolaDipintoDev"
+    },
+    { icon: PhoneOutlined, message: "Call me", url: "tel:+393914618137" },
+    {
+      icon: MailOutlined,
+      message: "dipinto.nicola.dev@gmail.com",
+      url: "mailto:dipinto.nicola.dev@gmail.com"
+    }
+  ];
