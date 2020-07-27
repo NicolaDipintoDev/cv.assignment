ce9312086a0dc7f75a79cb1400a0d322612a03a1
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-27 22:50:14 +0200

    render Email

--- cv-assignment/src/components/headerContent/headerIcons/HeaderIcons.js
+++ cv-assignment/src/components/headerContent/headerIcons/HeaderIcons.js
@@ -3,14 +3,22 @@ import { List } from "antd";
 import HeaderIcon from "../headerIcons/headerIcon/HeaderIcon";
 import LinkedIcon from "../headerIcons/headerIcon/LinkedIcon";
 import { icons } from "./constants/constants";
+import Email from "./headerIcon/Email";
 
 const HeaderIcons = () => {
-  const renderIcon = (index, item) =>
-    index < 2 ? (
-      <HeaderIcon Icon={item.icon} message={item.message} />
-    ) : (
-      <LinkedIcon Icon={item.icon} message={item.message} url={item.url} />
-    );
+  const renderIcon = (index, item) => {
+    if (index < 2) {
+      return <HeaderIcon Icon={item.icon} message={item.message} />;
+    }
+
+    if (index > 1 && index < 5) {
+      return (
+        <LinkedIcon Icon={item.icon} message={item.message} url={item.url} />
+      );
+    }
+
+    return <Email />;
+  };
 
   return (
     <List
--- cv-assignment/src/components/headerContent/headerIcons/headerIcon/HeaderIcon.js
+++ cv-assignment/src/components/headerContent/headerIcons/headerIcon/HeaderIcon.js
@@ -2,10 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Popover } from "antd";
 
-const HeaderIcon = ({ Icon, message }) => {
+const HeaderIcon = ({ Icon, message, onClick = null }) => {
   return (
     <Popover content={<span className="popoverHeader">{message}</span>}>
-      <Icon className="headerIcon" />
+      <Icon className="headerIcon" onClick={onClick} />
     </Popover>
   );
 };
@@ -15,4 +15,9 @@ export default HeaderIcon;
 HeaderIcon.propTypes = {
   Icon: PropTypes.object.isRequired,
   message: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+HeaderIcon.defaultProps = {
+  onClick: null,
 };
