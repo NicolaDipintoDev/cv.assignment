0763d1d91ea84ce6effa1d3a61af63cac179d170
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-27 21:39:22 +0200

    pass logged as props

--- cv-assignment/src/components/Content/container/rightSide/experiencesBlock/ExperiencesBlock.js
+++ cv-assignment/src/components/Content/container/rightSide/experiencesBlock/ExperiencesBlock.js
@@ -3,13 +3,19 @@ import PropTypes from "prop-types";
 import TimeLineComponent from "../timelinecomponent/TimeLineComponent";
 import Block from "../../../Block/Block";
 
-const ExperiencesBlock = ({ title, data }) => (
-  <Block Component={TimeLineComponent} title={title} items={data} />
+const ExperiencesBlock = ({ title, data, logged }) => (
+  <Block
+    Component={TimeLineComponent}
+    title={title}
+    items={data}
+    logged={logged}
+  />
 );
 
 export default ExperiencesBlock;
 
 ExperiencesBlock.propTypes = {
   title: PropTypes.string.isRequired,
-  data: PropTypes.array.isRequired
+  data: PropTypes.array.isRequired,
+  logged: PropTypes.bool.isRequired
 };
