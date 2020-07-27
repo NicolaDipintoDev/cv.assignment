25c884a0e47ae19436250859d067fd2f18b7d0c1
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-27 22:32:27 +0200

    remove unused imports

--- cv-assignment/src/components/Content/CollapseComponents/CollapseComponent.js
+++ cv-assignment/src/components/Content/CollapseComponents/CollapseComponent.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Collapse, Button, Popover } from "antd";
-import { PlusCircleOutlined } from "@ant-design/icons";
+import { Collapse } from "antd";
 import "./collapseComponent.css";
 import Title from "./title/Title";
 
