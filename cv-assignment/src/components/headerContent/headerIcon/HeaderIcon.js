0d713d20097eca25c8baafd25bea40c1b318aad2
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-27 20:22:38 +0200

    create HeaderIcon

---
+++ cv-assignment/src/components/headerContent/headerIcon/HeaderIcon.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Popover } from "antd";
+
+
+const HeaderIcon = ({Icon}) => {
+    
+    return (
+        <Popover
+        content={<span className="popoverHeader">{item.message}</span>}
+      >
+        <Icon className="headerIcon" />
+      </Popover>
+    );
+  };
+  
+  export default HeaderIcon;
+  
+  HeaderIcon.propTypes = {
+    Icon: PropTypes.func.isRequired
+  };
