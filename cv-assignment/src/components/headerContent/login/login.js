b9f0ec0d7fc964aab13e4bfbabcd26faefbce757
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-27 21:22:45 +0200

    change logged status

--- cv-assignment/src/components/headerContent/login/login.js
+++ cv-assignment/src/components/headerContent/login/login.js
@@ -1,20 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Popover } from 'antd';
+import { Button, Popover } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 
-const Login = ({logged, setLogged}) => {
+const Login = ({ logged, setLogged }) => {
+  const handleClick = () => setLogged(!logged);
   return (
     <span>
-       <Popover content={<span className="popoverHeader">Private area</span>}>
-      <Button ghost shape="circle" icon={<UserOutlined className="headerIcon"/>} />
+      <Popover content={<span className="popoverHeader">{logged ? 'Logout' : 'Private area'}</span>}>
+        <Button
+          ghost
+          shape="circle"
+          icon={<UserOutlined className="headerIcon" />}
+          onClick={handleClick}
+        />
       </Popover>
     </span>
   );
 };
 
 export default Login;
-
 
 Login.propTypes = {
   logged: PropTypes.bool.isRequired,
