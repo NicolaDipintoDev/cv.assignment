b3d5e7ba083a4b7ac0d093afb763145941a87fe9
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-27 21:25:23 +0200

    fix change icon when logged

--- cv-assignment/src/components/headerContent/login/login.js
+++ cv-assignment/src/components/headerContent/login/login.js
@@ -1,17 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button, Popover } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { UserOutlined, UserSwitchOutlined } from "@ant-design/icons";
 
 const Login = ({ logged, setLogged }) => {
   const handleClick = () => setLogged(!logged);
+  const Icon = logged ? UserSwitchOutlined : UserOutlined;
   return (
     <span>
       <Popover content={<span className="popoverHeader">{logged ? 'Logout' : 'Private area'}</span>}>
         <Button
           ghost
           shape="circle"
-          icon={<UserOutlined className="headerIcon" />}
+          icon={<Icon className="headerIcon" />}
           onClick={handleClick}
         />
       </Popover>
