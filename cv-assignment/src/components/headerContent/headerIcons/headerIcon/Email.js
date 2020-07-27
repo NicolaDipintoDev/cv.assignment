e5ac2f57189b87f80bcb79e200208ec5a2bac1a9
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-27 22:45:50 +0200

    email partial implemantion

--- cv-assignment/src/components/headerContent/headerIcons/headerIcon/Email.js
+++ cv-assignment/src/components/headerContent/headerIcons/headerIcon/Email.js
@@ -1,9 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { Input, Modal } from "antd";
 import { MailOutlined } from "@ant-design/icons";
+import HeaderIcon from "./HeaderIcon";
+
+const { TextArea } = Input;
 
 const Email = () => {
-const [massege, setMessage] = useState('');
-  return <div> email </div>;
+  const [massege, setMessage] = useState("");
+  return (
+    <>
+      <HeaderIcon
+        Icon={MailOutlined}
+        message="dipinto.nicola.dev@gmail.com"
+        onClick={() => console.log('mail clicked')}
+      />
+    </>
+  );
 };
 
 export default Email;
