116a33e26915ab9cf385471b7b551ad9bb912a17
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-27 20:56:34 +0200

    fix phone icon message

--- cv-assignment/src/components/headerContent/headerIcons/constants/constants.js
+++ cv-assignment/src/components/headerContent/headerIcons/constants/constants.js
@@ -6,7 +6,7 @@ import {
     MailOutlined,
     PhoneOutlined
   } from "@ant-design/icons";
-  
+
 export const icons = [
     { message: "20/05/1993", icon: CalendarOutlined },
     { message: "Limbiate (MB)", icon: EnvironmentOutlined },
@@ -20,7 +20,7 @@ export const icons = [
       message: "GitHub",
       url: "https://github.com/NicolaDipintoDev"
     },
-    { icon: PhoneOutlined, message: "Call me", url: "tel:+393914618137" },
+    { icon: PhoneOutlined, message: "3914618137", url: "tel:+393914618137" },
     {
       icon: MailOutlined,
       message: "dipinto.nicola.dev@gmail.com",
